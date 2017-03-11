398b504212ac249bcd5e07b48a028c54b094a9a2
WINDOWS-O9VI1QM\rama4 <ramakrishna.candy@gmail.com>
2017-03-11 14:10:24 -0500

    changes to courses.js file

--- scripts/courses.js
+++ scripts/courses.js
@@ -6,13 +6,13 @@ var allCourses =  [ {StudentName: "RK", CourseName: "HTML", Duration: 15, Course
 
 function saveCourse()
 {
-	var studentName = document.calc.name.value;
-	var courseName = document.calc.course.value;
-	var duration =document.calc.day.value;
+	var StudentName = document.calc.name.value;
+	var CourseName = document.calc.course.value;
+	var Duration =document.calc.day.value;
 	var courseType =document.calc.tc.options[1].selected;
 	var isCompleted =document.calc.radioCompleted[0].checked;
 	
-	allCourses[allCourses.length] = {studentName, courseName, duration, courseType, isCompleted};
+	allCourses[allCourses.length] = {StudentName, CourseName, Duration, courseType, isCompleted};
 	var courseLength=allCourses.length;
 	
 	for(var loop=0; loop < courseLength; loop++)  
